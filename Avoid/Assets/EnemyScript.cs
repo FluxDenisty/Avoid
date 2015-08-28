162090937260126e1a342f2b278963d5b440f6ed
Ryan <ryanbateman.uw@gmail.com>
2015-08-27 20:20:25 -0400

    I MADE A BORING GAME

---
+++ Avoid/Assets/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+	
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 diff = PlayerScript.instance.transform.position - this.transform.position;
+
+		this.rigidBody.AddForce(diff * 5.0f);
+		if (this.rigidBody.velocity.magnitude > 4.0f) {
+			this.rigidBody.velocity = this.rigidBody.velocity * (4f / this.rigidBody.velocity.magnitude);
+		}
+	}
+}
---
+++ Avoid/Assets/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public static PlayerScript instance;
+
+	public Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidBody = this.GetComponent<Rigidbody2D>();
+		PlayerScript.instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 diff = Vector2.zero;
+		if (Input.GetKey(KeyCode.W)) {
+			diff += Vector2.up;
+		} else if (Input.GetKey(KeyCode.A)) {
+			diff += Vector2.left;
+		} else if (Input.GetKey(KeyCode.S)) {
+			diff += Vector2.down;
+		} else if (Input.GetKey(KeyCode.D)) {
+			diff += Vector2.right;
+		}
+
+		this.rigidBody.AddForce(diff * 10.0f);
+		if (this.rigidBody.velocity.magnitude > 4.5f) {
+			this.rigidBody.velocity = this.rigidBody.velocity * (4f / this.rigidBody.velocity.magnitude);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Enemy") {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
